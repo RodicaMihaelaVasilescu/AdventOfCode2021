9598277a207f12d8be777d6650fdb5c811dae1e7
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2021-12-08 10:00:23 +0200

    --- Day 8: Seven Segment Search ---

--- Day 08/Day 8.cpp
+++ Day 08/Day 8.cpp
@@ -1,16 +1,10 @@
-//to do: I'll rewrite this
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <cstdio>
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <map>
-#include <set>
-#include <regex>
-#include <sstream>
 
 using namespace std;
 
@@ -30,9 +24,56 @@ vector<string> split(string line, string delimiters = ",./;")
     index = line.find_first_of(delimiters);
     auto newWord = line.substr(0, index);
     if (!newWord.empty())
+    {
       words.push_back(newWord);
+    }
   }
   return words;
+}
+
+int segmentToInt(string segment)
+{
+  sort(segment.begin(), segment.end());
+  if (segment == "abcefg")
+  {
+    return 0;
+  }
+  else if (segment == "cf")
+  {
+    return 1;
+  }
+  else  if (segment == "acdeg")
+  {
+    return 2;
+  }
+  else  if (segment == "acdfg")
+  {
+    return  3;
+  }
+  else  if (segment == "bcdf")
+  {
+    return  4;
+  }
+  else  if (segment == "abdfg")
+  {
+    return  5;
+  }
+  else  if (segment == "abdefg")
+  {
+    return  6;
+  }
+  else  if (segment == "acf")
+  {
+    return 7;
+  }
+  else  if (segment == "abcdefg")
+  {
+    return  8;
+  }
+  else  if (segment == "abcdfg")
+  {
+    return  9;
+  }
 }
 
 int main()
@@ -40,39 +81,27 @@ int main()
   freopen("in.txt", "r", stdin);
   freopen("out.txt", "w", stdout);
 
-  string line;
   map<char, char> m;
-  set<string> configurations;
-  long long s = 0;
-  map<int, int> lettersFr;
-  int sum2 = 0;
+
+  long long sumPart1 = 0;
+  long long sumPart2 = 0;
+
+  string line;
   while (getline(cin, line)) {
 
     map<char, int> lettersFrequency;
-    map<string, int> wordSize;
 
-    auto input_output = split(line, "|");
-    auto words = split(input_output[0], " ");
-    auto outputWords = split(input_output[1], " ");
+    auto lineWords = split(line, "|");
+    auto theFirst10Words = split(lineWords[0], " ");
+    auto theLast4Words = split(lineWords[1], " ");
 
     string fourLetter;
     string eightLetter;
     string oneLetter;
 
-    for (auto& word : outputWords)
+    for (auto& word : theFirst10Words)
     {
       sort(word.begin(), word.end());
-      if (word.size() == 4 || word.size() == 7 || word.size() == 2 || word.size() == 3)
-      {
-        sum2++;
-      }
-    }
-
-    for (auto& word : words)
-    {
-      sort(word.begin(), word.end());
-      configurations.insert(word);
-      wordSize[word] = word.size();
       if (word.size() == 4)
       {
         fourLetter = word;
@@ -89,26 +118,14 @@ int main()
       for (auto letter : word)
       {
         lettersFrequency[letter]++;
-
       }
     }
 
     for (auto letter : lettersFrequency)
     {
-      if (letter.second == 9)
-      {
-        m[letter.first] = 'f';
-        lettersFrequency[letter.first] = 0;
-      }
       if (letter.second == 6)
       {
         m[letter.first] = 'b';
-        lettersFrequency[letter.first] = 0;
-      }
-      if (letter.second == 4)
-      {
-        m[letter.first] = 'e';
-        lettersFrequency[letter.first] = 0;
       }
     }
     for (auto letter : oneLetter)
@@ -116,12 +133,10 @@ int main()
       if (lettersFrequency[letter] == 9)
       {
         m[letter] = 'f';
-        lettersFrequency[letter] = 0;
       }
       if (lettersFrequency[letter] == 8)
       {
         m[letter] = 'c';
-        lettersFrequency[letter] = 0;
       }
 
     }
@@ -160,77 +175,24 @@ int main()
 
     }
 
-    vector<string> newWords;
-    map<string, int> nrs;
-    int nr = 0;
-    for (auto word : outputWords)
+    int numberOfFourDigits = 0;
+    for (auto word : theLast4Words)
     {
       string newWord = "";
       for (auto letter : word)
       {
         newWord += m[letter];
       }
-      newWords.push_back(newWord);
-      sort(newWord.begin(), newWord.end());
-      if (newWord == "abcefg")
+      int number = segmentToInt(newWord);
+      numberOfFourDigits = numberOfFourDigits * 10 + number;
+      if (number == 1 || number == 4 || number == 7 || number == 8)
       {
-        nr = nr * 10 + 0;
-        nrs[newWord] = 0;
-      }
-      else  if (newWord == "cf")
-      {
-        nr = nr * 10 + 1;
-        nrs[newWord] = 1;
-      }
-      else  if (newWord == "acdeg")
-      {
-        nr = nr * 10 + 2;
-        nrs[newWord] = 2;
-      }
-      else  if (newWord == "acdfg")
-      {
-        nr = nr * 10 + 3;
-        nrs[newWord] = 3;
-      }
-      else  if (newWord == "bcdf")
-      {
-        nr = nr * 10 + 4;
-        nrs[newWord] = 4;
-      }
-      else  if (newWord == "abdfg")
-      {
-        nr = nr * 10 + 5;
-        nrs[newWord] = 5;
-      }
-      else  if (newWord == "abdefg")
-      {
-        nr = nr * 10 + 6;
-        nrs[newWord] = 6;
-      }
-      else  if (newWord == "acf")
-      {
-        nr = nr * 10 + 7;
-        nrs[newWord] = 7;
-      }
-      else  if (newWord == "abcdefg")
-      {
-        nr = nr * 10 + 8;
-        nrs[newWord] = 8;
-      }
-      else  if (newWord == "abcdfg")
-      {
-        nr = nr * 10 + 9;
-        nrs[newWord] = 9;
-      }
-      else
-      {
-        int bkp = 0;
+        sumPart1++;
       }
     }
-    s += nr;
-
+    sumPart2 += numberOfFourDigits;
   }
 
-  cout << "Part 2: " << s << endl;
-
+  cout << "Part 1: " << sumPart1 << endl;
+  cout << "Part 2: " << sumPart2 << endl;
 }
