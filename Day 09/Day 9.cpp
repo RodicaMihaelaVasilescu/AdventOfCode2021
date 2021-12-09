5bdcf30fea4e476a6ae374625c23d031e7410cf4
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2021-12-09 09:41:52 +0200

    --- Day 9: Smoke Basin ---

--- Day 09/Day 9.cpp
+++ Day 09/Day 9.cpp
@@ -1,30 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <cstdio>
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <map>
 #include <set>
 #include <queue>
-#include <sstream>
 
 using namespace std;
-
-void replaceAll(string& s, const string& search, const string& replace)
-{
-  for (size_t pos = 0; ; pos += replace.length())
-  {
-    // Locate the substring to replace
-    pos = s.find(search, pos);
-    if (pos == string::npos) break;
-    // Replace by erasing and inserting
-    s.erase(pos, search.length());
-    s.insert(pos, replace);
-  }
-}
 
 vector<string> split(string line, string delimiters = " ,./;")
 {
